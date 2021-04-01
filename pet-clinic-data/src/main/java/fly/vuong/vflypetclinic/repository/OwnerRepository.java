d129199428c04995a7f8a2631ba9a75ac58ba737
vuongfly <vuongfly96@gmail.com>
2021-04-01 23:31:55 +0700

    update owner

--- pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/repository/OwnerRepository.java
@@ -2,6 +2,8 @@
 //
 //import fly.vuong.vflypetclinic.model.Owner;
 //import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
 //
+//@Repository
 //public interface OwnerRepository extends JpaRepository<Owner, Long> {
 //}
--- pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/service/implJpa/OwnerServiceImplJpa.java
+++ pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/service/implJpa/OwnerServiceImplJpa.java
@@ -2,7 +2,9 @@ package fly.vuong.vflypetclinic.service.implJpa;
 
 import fly.vuong.vflypetclinic.model.Owner;
 import fly.vuong.vflypetclinic.service.OwnerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OwnerServiceImplJpa implements OwnerService {
     @Override
     public void addOwner(Owner owner) {
