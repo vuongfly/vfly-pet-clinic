5c75bfe17ca4ff6ee37ffaca3deebc2f8d89f985
vuongfly <vuongfly96@gmail.com>
2021-04-01 16:36:49 +0700

    add vet index and vet controller

---
+++ pet-clinic-web/src/main/java/fly/vuong/vflypetclinic/controller/VetController.java
@@ -0,0 +1,14 @@
+package fly.vuong.vflypetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index"})
+    public String listVets(){
+        return "vets/index";
+    }
+
+}
