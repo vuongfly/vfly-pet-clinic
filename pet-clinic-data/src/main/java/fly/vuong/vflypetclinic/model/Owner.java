16d83952c3230ec57f4f79c71068155eda972296
vuongfly <vuongfly96@gmail.com>
2021-04-02 13:49:58 +0700

    update code, try to fix but not work

--- pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/model/Owner.java
@@ -4,9 +4,11 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
 @NoArgsConstructor
+@Component
 public class Owner extends Person{
 }
--- pet-clinic-web/src/main/java/fly/vuong/vflypetclinic/VflyPetClinicApplication.java
+++ pet-clinic-web/src/main/java/fly/vuong/vflypetclinic/VflyPetClinicApplication.java
@@ -2,8 +2,14 @@ package fly.vuong.vflypetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"fly.vuong"})
+//@EnableJpaRepositories(basePackages={"fly.vuong.vflypetclinic.repository"})
+//@EntityScan(basePackages={"fly.vuong.vflypetclinic.model"})
 public class VflyPetClinicApplication {
 
     public static void main(String[] args) {
