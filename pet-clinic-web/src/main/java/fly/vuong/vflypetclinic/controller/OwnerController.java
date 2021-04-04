63c32c0919c55bd17574f09318b66970415c6f4e
vuongfly <vuongfly96@gmail.com>
2021-04-04 21:08:09 +0700

    Creat owner index page to display all owner through OwnerController

--- pet-clinic-web/src/main/java/fly/vuong/vflypetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/fly/vuong/vflypetclinic/controller/OwnerController.java
@@ -1,14 +1,25 @@
 package fly.vuong.vflypetclinic.controller;
 
+import fly.vuong.vflypetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owner")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index"})
-    public String listOwner(){
+    public String listOwner(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 
