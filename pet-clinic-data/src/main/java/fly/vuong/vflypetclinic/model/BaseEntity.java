cebc509f1f367e5b86dbedd92409b8ac835bca47
vuongfly <vuongfly96@gmail.com>
2021-04-12 09:31:16 +0700

    implement BaseEntity with JPA annotation

--- pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/model/BaseEntity.java
@@ -3,10 +3,17 @@ package fly.vuong.vflypetclinic.model;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Data
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 }
