e65b771037d62a7606622aa1e527a82ae570eb47
vuongfly <vuongfly96@gmail.com>
2021-04-14 16:04:18 +0700

    add PetServiceJpaImpl

---
+++ pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/service/jpaIpml/PetServiceJpaImpl.java
@@ -0,0 +1,48 @@
+package fly.vuong.vflypetclinic.service.jpaIpml;
+
+import fly.vuong.vflypetclinic.model.Pet;
+import fly.vuong.vflypetclinic.repository.PetRepository;
+import fly.vuong.vflypetclinic.service.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetServiceJpaImpl implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceJpaImpl(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
