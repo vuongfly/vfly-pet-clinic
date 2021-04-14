0d577fde154f41d5dbdd06eabc21b60985b50808
vuongfly <vuongfly96@gmail.com>
2021-04-14 15:59:37 +0700

    impl VisitService with JPA

---
+++ pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package fly.vuong.vflypetclinic.service;
+
+import fly.vuong.vflypetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/fly/vuong/vflypetclinic/service/jpaIpml/VisitServiceJpaImpl.java
@@ -0,0 +1,44 @@
+package fly.vuong.vflypetclinic.service.jpaIpml;
+
+import fly.vuong.vflypetclinic.model.Visit;
+import fly.vuong.vflypetclinic.repository.VisitRepository;
+import fly.vuong.vflypetclinic.service.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VisitServiceJpaImpl implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitServiceJpaImpl(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
